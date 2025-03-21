eb292364e56e32d4d33b3b35c329ddc55db5fa64
aihddelyy <aihdce@gmail.com>
2025-03-21 11:09:52 +0800

    up

--- .github/diy/packages.sh
+++ .github/diy/packages.sh
@@ -55,13 +55,13 @@ git clone --depth 1 https://github.com/muink/luci-app-homeproxy
 # git clone --depth 1 https://github.com/tty228/luci-app-wechatpush
 # git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata
-git clone --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+git clone --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic
 # git clone --depth 1 https://github.com/BROBIRD/openwrt-r8168
 # git clone --depth 1 https://github.com/sbwml/package_kernel_r8125
 git clone --depth 1 https://github.com/sbwml/luci-app-filemanager
 # git clone --depth 1 https://github.com/schen39/luci-app-serverchan
 
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages ./relevance/passwall-packages
 # git clone --depth 1 -b main https://github.com/fw876/helloworld && mvdir helloworld
 # git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go && mvdir luci-app-ddns-go
 
@@ -71,11 +71,11 @@ git clone --depth 1 https://github.com/kenzok8/wall && mv -n wall/adguardhome wa
 # git clone --depth 1 https://github.com/xiangfeidexiaohuo/extra-ipk && mv -n extra-ipk/patch/wall-luci/luci-app-vssr ./ ; rm -rf extra-ipk
 # git clone --depth 1 https://github.com/sbwml/luci-app-alist openwrt-alist && mv -n openwrt-alist/*alist ./ ; rm -rf openwrt-alist
 git clone --depth 1 https://github.com/lisaac/luci-app-dockerman dockerman && mv -n dockerman/applications/luci-app-dockerman ./ ; rm -rf dockerman
-git clone --depth 1 https://github.com/sbwml/packages_lang_golang && mv -n packages_lang_golang/golang ./ ; rm -rf packages_lang_golang
+git clone --depth 1 https://github.com/sbwml/packages_lang_golang && mv -n packages_lang_golang/golang ./relevance/ ; rm -rf packages_lang_golang
 git clone --depth 1 https://github.com/linkease/istore luci-app-istore
 git clone --depth 1 https://github.com/linkease/nas-packages-luci nas-packages-luci && mv -n nas-packages-luci/luci/luci-app-quickstart ./ ; rm -rf nas-packages-luci
-git clone --depth 1 https://github.com/linkease/nas-packages nas-packages && mv -n nas-packages/network/services/quickstart ./ ; rm -rf nas-packages
-git clone --depth 1 https://github.com/sirpdboy/luci-app-lucky sirpdboy-lucik && mv -n sirpdboy-lucik/*lucky ./ ; rm -rf sirpdboy-lucik
+git clone --depth 1 https://github.com/linkease/nas-packages nas-packages && mv -n nas-packages/network/services/quickstart ./relevance/ ; rm -rf nas-packages
+git clone --depth 1 https://github.com/sirpdboy/luci-app-lucky sirpdboy-lucik && mv -n sirpdboy-lucik/*lucky ./ && mv -n lucky ./relevance/ ; rm -rf sirpdboy-lucik
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns luci-app-mosdns
 # git clone --depth 1 https://github.com/sbwml/luci-app-qbittorrent sirpdboy-qbittorrent && mv -n sirpdboy-qbittorrent/* ./ ; rm -rf sirpdboy-qbittorrent
 # git clone --depth 1 https://github.com/messense/aliyundrive-webdav aliyundrive && mv -n aliyundrive/openwrt/* ./ ; rm -rf aliyundrive
@@ -83,15 +83,15 @@ git clone --depth 1 https://github.com/sbwml/luci-app-mosdns luci-app-mosdns
 # git clone --depth 1 https://github.com/kiddin9/kwrt-packages && mv -n kwrt-packages/luci-app-bypass kwrt-packages/luci-app-fileassistant ./ ; rm -rf kwrt-packages
 # git clone --depth 1 https://github.com/immortalwrt/packages && mv -n packages/net/{cdnspeedtest,dae,daed,vsftpd,transmission} ./ ; rm -rf packages
 git clone --depth 1 https://github.com/immortalwrt/luci && mv -n luci/applications/{luci-app-filebrowser,luci-app-gost,luci-app-usb-printer,luci-app-vlmcsd,luci-app-transmission} ./ ; rm -rf luci
-git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki OpenWrt-nikki && mv -n OpenWrt-nikki/*nikki ./ ; rm -rf OpenWrt-nikki
+git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki OpenWrt-nikki && mv -n OpenWrt-nikki/*nikki ./ && mv -n nikki ./relevance/ ; rm -rf OpenWrt-nikki
 # git clone --depth 1 https://github.com/muink/openwrt-fchomo openwrt-fchomo && mv -n openwrt-fchomo/*homo ./ ; rm -rf openwrt-fchomo
-git clone --depth 1 https://github.com/kiddin9/kwrt-packages kwrt-packages && mv -n kwrt-packages/*adbyby* ./ ; rm -rf kwrt-packages
+git clone --depth 1 https://github.com/kiddin9/kwrt-packages kwrt-packages && mv -n kwrt-packages/*adbyby* ./ && mv -n adbyby ./relevance/ ; rm -rf kwrt-packages
 
 git clone --depth 1 -b dev https://github.com/vernesong/OpenClash && mv -n OpenClash/luci-app-openclash ./ ; rm -rf OpenClash
 git_sparse_clone openwrt-24.10 "https://github.com/immortalwrt/luci" applications/luci-app-argon-config
 
 # 清理目录
-mkdir -p relevance
-mv -f `find ./* -maxdepth 0 -type d ! -name "luci-app*"` ./relevance/ >/dev/null 2>&1
+# mkdir -p relevance
+# mv -f `find ./* -maxdepth 0 -type d ! -name "luci-app*"` ./relevance/ >/dev/null 2>&1
 rm -rf ./*/.git & rm -rf ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
